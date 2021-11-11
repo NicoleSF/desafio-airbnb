cab2b25498e75ebcb70908fc2b82a6647df2caec
Ketryn Silva <78522164+ketrynsdm@users.noreply.github.com>
2021-11-11 16:31:28 -0300

    conectado com mongodb

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,35 +1,45 @@
-import { connect, disconnect } from 'mongoose';
-import { PessoaModel } from './mongo';
+//import { connect, disconnect } from 'mongoose';
+import { MongoClient } from 'mongodb';
+//import { PessoaModel } from './mongo';
 
-const uri='mongodb+srv://ProjetoDB:Dbserver@cluster0.tqbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const uri='mongodb+srv://ProjetoDB:Dbserver@cluster0.tqbkf.mongodb.net/ProjetoDB?retryWrites=true&w=majority';
+const uri = 'mongodb://projetoMongo:dbserver@cluster0-shard-00-00.a1fww.mongodb.net:27017,cluster0-shard-00-01.a1fww.mongodb.net:27017,cluster0-shard-00-02.a1fww.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-p1jav0-shard-0&authSource=admin&retryWrites=true&w=majority';
+const conectCadastro = new MongoClient (uri);
 
-// const conectCadastro = new MongoClient (uri);
+
 
 
 
 
 async function main() {
     try {
-        await connect(uri);
-        // await conectCadastro.connect();
+        //await connect(uri);
+        await conectCadastro.connect();
         console.log('Conectado ao MongoDb Atlas');
 
-    //     const documentoInserido = await CadastroModel.create({
-    //         nome: 'John Doe',
-    //         idade: 25,
-    //         email: 'jd@gmail.com'
-    //     });
         
 
-    //     const basededados = conectCadastro.db('Projetobd');
-    //     const colecao = basededados.collection<Cadastro>('cadastros');
+         /*const documentoInserido = await CadastroModel.create({
+             nome: 'John Doe',
+             idade: 25,
+             email: 'jd@gmail.com'
+         });
+        
+         const basededados = conectCadastro.db('Projetobd');
+         const colecao = basededados.collection<Cadastro>('cadastros');*/
+        
+        console.log('Conectado ao MongoDb Atlas');
+
+   
     }
         catch (error) {
             console.log('Falha de acesso ao BD:');
             console.error(error);
+            
         } finally {
-            await disconnect();
-            // await conectCadastro.close();
+            //await disconnect();
+            await conectCadastro.close();
+        
         }
 }
 
