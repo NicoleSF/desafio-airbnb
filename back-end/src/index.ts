8cdebc690f6f5f9e3066a921df39a5d589d98cc4
Nicole Siqueira Fagundes <psi.fagundes@gmail.com>
2021-11-12 09:22:35 -0300

    listaHospedagem

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,6 +2,7 @@ import { connect, disconnect } from 'mongoose';
 //import { MongoClient } from 'mongodb';
 import { Cadastro } from './mongo';
 import { CadastroModel } from './mongo';
+import { HospedagemModel } from './mongo';
 
 
 //const uri='mongodb+srv://ProjetoDB:Dbserver@cluster0.tqbkf.mongodb.net/ProjetoDB?retryWrites=true&w=majority';
@@ -32,14 +33,35 @@ async function main() {
         console.log(`Inserido: ${resultado.insertedId}`);*/
 
         
-         /*const documentoInserido = await CadastroModel.create({
+ /* const documentoInserido = await CadastroModel.create({
              nome: 'John Doe',
-             idade: 25,
-             email: 'jd@gmail.com'
+             email: 'jd@gmail.com', 
+             telefone: 99999999,
+             endereco: 'Rua John 55',
+             senha: 'dbserver'
          });
+         console.log('Inserido: ');
+         console.log(documentoInserido);*/
+
+         await HospedagemModel.create({
+             nome: 'Casa da Colina',
+             resumo: 'Uma casa linda e aconchegante localizada nas colinas da serra gaúcha. Ambiente calmo e tranquilo, sons de pássaros e natureza',
+             caracteristica: 'Acesso ao lago, wi-fi, permitido animais, quintal, cozinha, pátio ou varanda, lareira interna'
+         },
+         {
+             nome: 'Apartamento de frente para o Mar',
+             resumo: 'Um belo apartamento bem localizado com todo o equipamento necessário para passar uma temporada na praia',
+             caracteristica: 'cozinha, wi-fi, garagem, permitido animais, máquina de lavar roupa, netflix'
+         }, {
+             nome: 'Loft no centro da Cidade',
+             resumo: 'Loft com 200m²m localizado no coração da cidade. Possui padaria, supermercado, farmácia, tudo perto',
+             caracteristica: 'wi-fi, cozinha, máquina de lavar, netflix, garagem, permite animais,'
+         });
+
+         
         
 
-         const basededados = conectCadastro.db('Projetobd');
+         /*const basededados = conectCadastro.db('Projetobd');
          const colecao = basededados.collection<Cadastro>('cadastros');*/
         
     }
